8085fe300910de68393f820e2893aa1d2bfcfd49
vivianebresolin <vivianebresolin@gmail.com>
2021-05-05 16:07:14 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperior/dashboard/DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
