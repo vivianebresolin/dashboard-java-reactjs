271966121d5c92f6b53a6da07f1c3b0eca58cf8c
vivianebresolin <vivianebresolin@gmail.com>
2021-05-05 17:34:18 -0300

    Added static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,17 @@
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+
 function App() {
   return (
-    <h1 className="text-primary">App</h1>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">App</h1>
+      </div>
+      <DataTable />
+      <Footer />
+    </>
   );
 }
 
