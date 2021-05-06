9f4c28207042bcce61aa4992329a474aa53af329
vivianebresolin <vivianebresolin@gmail.com>
2021-05-06 12:17:10 -0300

    Added pagination to Get Sales

---
+++ backend/src/main/java/com/devsuperior/dashboard/services/SaleService.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dashboard.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dashboard.dto.SaleDTO;
+import com.devsuperior.dashboard.entities.Sale;
+import com.devsuperior.dashboard.repositories.SaleRepository;
+import com.devsuperior.dashboard.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+
+	}
+}
