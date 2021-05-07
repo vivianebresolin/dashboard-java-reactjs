9f8a1c2e7da50760b663b71b3c5c2a54bacc5b2a
vivianebresolin <vivianebresolin@gmail.com>
2021-05-07 15:05:04 -0300

    Integrated BarChart with the backend

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -2,3 +2,9 @@ export type SaleTotalPerSeller = {
     sellerName: string,
     total: number
 };
+
+export type ConversionRatePerSeller = {
+    sellerName: string,
+    visited: number,
+    deals: number
+}
